16a6adae40fe4d7e20289900d22f6e2012c35727
trickstarcandina <rinnegan101220@gmail.com>
2024-01-06 22:43:56 +0700

    add client type continuous

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config({ path: "./.env" });
 const { Client } = require("discord.js-selfbot-v13");
-const { runClient } = require("./clientType");
+const { runClient } = require("./clientSchedule");
 
 try {
     runClient(
